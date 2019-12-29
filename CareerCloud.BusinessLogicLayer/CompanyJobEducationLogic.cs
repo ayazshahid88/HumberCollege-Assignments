9168b9542529517a8138eff2fce448666d67a94e
Ayaz Shahid <ayaz.shahid@yahoo.com>
2019-12-29 17:19:33 -0500

    Assignment 3

---
+++ CareerCloud.BusinessLogicLayer/CompanyJobEducationLogic.cs
@@ -0,0 +1,47 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyJobEducationLogic : BaseLogic<CompanyJobEducationPoco>
+    {
+        public CompanyJobEducationLogic(IDataRepository<CompanyJobEducationPoco> repository)
+            : base(repository)
+        {
+
+        }
+        protected override void Verify(CompanyJobEducationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (CompanyJobEducationPoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Major) || poco.Major.Length < 2)
+                {
+                    exceptions.Add(new ValidationException(200, "The value for Major cannot be empty or less than 2 characters."));
+                }
+                if (poco.Importance < 0)
+                {
+                    exceptions.Add(new ValidationException(201, "The value for Importance cannot be less than 0."));
+                }
+            }
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        public override void Add(CompanyJobEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+        public override void Update(CompanyJobEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+    }
+}
---
+++ CareerCloud.BusinessLogicLayer/CompanyProfileLogic.cs
@@ -0,0 +1,69 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyProfileLogic : BaseLogic<CompanyProfilePoco>
+    {
+        public CompanyProfileLogic(IDataRepository<CompanyProfilePoco> repository)
+            : base(repository)
+        {
+
+        }
+
+        protected override void Verify(CompanyProfilePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+
+            foreach (CompanyProfilePoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.CompanyWebsite))
+                {
+                    exceptions.Add(new ValidationException(600, "Company Website field should not be blank."));
+                }
+
+               else if (!poco.CompanyWebsite.EndsWith(".com") && !poco.CompanyWebsite.EndsWith(".ca") && !poco.CompanyWebsite.EndsWith(".biz"))
+                {
+                    exceptions.Add(new ValidationException(600, "Valid websites must end with the \".ca\", \".com\", \".biz\"."));
+                }
+
+                if (string.IsNullOrEmpty(poco.ContactPhone))
+                {
+                    exceptions.Add(new ValidationException(601, "Phone number must be in a valid format (e.g. 416-555-1234)"));
+                }
+
+                else
+                {
+                    String[] phoneNumberElement = poco.ContactPhone.Split("-");
+                    if (phoneNumberElement.Length != 3)
+                    {
+                        exceptions.Add(new ValidationException(601, "Phone Number must be in a valid format (e.g. 416-555-1234)"));
+                    }
+
+                    else if ((phoneNumberElement[0].Length != 3) || (phoneNumberElement[1].Length != 3) || (phoneNumberElement[2].Length != 4))
+                    {
+                        exceptions.Add(new ValidationException(601, "Phone Number must be in a valid format (e.g. 416-555-1234)"));
+                    }
+                }
+            }
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        public override void Add(CompanyProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+        public override void Update(CompanyProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+    }
+}
